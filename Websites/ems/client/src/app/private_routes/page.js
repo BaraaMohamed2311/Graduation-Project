655a33dc03410f3613ab48c76b4dc7bf714b90d0
Baraa Mohamed <baraamohamed2311@gmail.com>
2026-02-21 17:22:01 +0200

    test pipeline and github actions

--- Websites/ems/client/src/app/private_routes/page.js
+++ Websites/ems/client/src/app/private_routes/page.js
@@ -22,7 +22,7 @@ export default function private_routes(WrappedComponent) {
  
     useEffect(() => {
       if (!isLoading && !isValid) {
-        router.push("/login"); // Navigate to login
+        router.push("/login"); // Navigate to login page
       }
     }, [isLoading, isValid]);
 
