42694f7b0eba7b977e64e0c51710e46b2780c114
Baraa Mohamed <baraamohamed2311@gmail.com>
2026-03-20 20:53:20 +0200

    test fixing ems client container

--- Websites/ems/client/next.config.mjs
+++ Websites/ems/client/next.config.mjs
@@ -2,7 +2,7 @@
 
 const nextConfig = { 
     env: {
-        // "http://localhost:3050/api" so localhost uses nginx to communcate between containers inside docker
+        // "http://localhost:3050/api" so localhost uses nginx to communcate between containers inside docker, and "/api" so production uses nginx to communcate between frontend and backend
         APIKEY: process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5500/api",
       },
       reactStrictMode: false,
